22ca5c660ad443c2ab01404982c5c013d3484d38
JediKnight <jediknight@harrisonlogic.com>
2018-02-20 06:27:46 -0800

    Week 1

---
+++ PRG420/src/week_1/PRG420Week1_ASM2.java
@@ -0,0 +1,44 @@
+/**********************************************************************
+*	Program:	 PRG420Week1_CodingAssignment	
+* 	Purpose: 	 Week 1 Individual Assignment #2	
+*	Programmer:	 YOUR FULL NAME GOES HERE		
+*	Class:		 PRG/420			
+*	Creation Date:	 TODAY'S DATE GOES HERE
+*********************************************************************
+*
+**********************************************************************
+*	Program Summary: This program asks the user for a one-word name
+*                        and then displays a constructed greeting
+*                        that looks like this: 
+*
+*                        Hello, NAME!
+**********************************************************************/
+
+package week_1;
+
+/**
+ *
+ * @author NETBEANS PLACES YOUR INITIALS HERE
+ */
+
+
+import java.util.Scanner;
+
+public class PRG420Week1_ASM2 {
+    
+    public static void main(String[] args) {
+        // Create a usable instance of an input device    
+        LINE 1. INSTANTIATE AN INSTANCE OF SCANNER AND ASSIGN IT TO A VARIABLE OF TYPE SCANNER. 
+
+        // Prompt user for input
+        LINE 2. USE THE PRINT() METHOD TO PROMPT THE USER FOR HIS OR HER FIRST NAME.
+
+        // Capture first word and assign it to A VARIABLE
+        LINE 3. USE THE NEXT() METHOD OF YOUR SCANNER INSTANCE TO ASSIGN A VALUE TO A STRING VARIABLE.
+
+        // Construct the greeting 
+        LINE 4. USE THE PRINTLN() METHOD TO CONSTRUCT A CORRECTLY PUNCTUATED GREETING.
+
+    }
+    
+}
