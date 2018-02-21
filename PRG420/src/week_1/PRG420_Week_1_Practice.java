fbee1b23d54b97f9ea34308a0ee1baf8f8c66081
Jedi Knight <jediknight@harrisonlogic.com>
2018-02-21 15:25:01 -0800

    Week 1

--- PRG420/src/week_1/PRG420_Week_1_Practice.java
+++ PRG420/src/week_1/PRG420_Week_1_Practice.java
@@ -5,7 +5,7 @@ public class PRG420_Week_1_Practice {
 	public static void main(String[] args) {
 		
 		// Print the greeting to the screen
-		System.out.println("Welcome to PRG420!!"); 
+		System.out.println("Welcome to PRG420!!!"); 
 
 	}
 
